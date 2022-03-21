05f472897dbeab9d3fe1ab646700d5fd1cc9575b
Daniel Silva <daniel.silva@onlineapp.com.br>
2022-03-21 17:08:20 -0300

    Module

---
+++ Project-Angular/src/app/cursos/cursos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageCursoComponent } from '../page-curso/page-curso.component';
+
+
+
+@NgModule({
+  declarations: [
+    PageCursoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CursosModule { }
---
+++ Project-Angular/src/app/page-curso/page-curso.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageCursoComponent } from './page-curso.component';
+
+describe('PageCursoComponent', () => {
+  let component: PageCursoComponent;
+  let fixture: ComponentFixture<PageCursoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PageCursoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageCursoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
